ca10d365617905c79367dd4182c9d35cfe4fe70a
Sarah-kundu <Sarah.23804@stu.upes.ac.in>
2025-12-04 00:12:59 +0530

    Add files via upload

--- Project/project.c
+++ Project/project.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+
+typedef struct passenger { //creating a structure for passanger info
+    char name[30];
+    int age;
+    char gender[10];
+    char pref[10];
+} passenger;
+
 int main(){
     char SOURCE[20];  // creating character array
     char DESTINATION[20];
     float TICKET_PRICE= 2300; //the amount of the ticket will be stored here
     int AVAILABLE_SEATS= 50; //this shows the number of tickets that available
     char date_of_journey[15]; //stores the date of journey
-    char passenger_name[30]; //passenger's credentials
-    int passenger_age;
-    char passenger_gender[10];
     
+    
+    passenger p;
+
     printf("Search Train \n"); //searching the train
     printf("Route\nTo: "); //  taking input of desired destination from the user
-    scanf("%s", &DESTINATION); 
+    fgets(DESTINATION, sizeof(DESTINATION), stdin);
+    DESTINATION[strcspn(DESTINATION, "\n")] = 0;
+
     printf("From: "); 
-    scanf("%s",&SOURCE);
+    fgets(SOURCE, sizeof(SOURCE), stdin);
+    SOURCE[strcspn(SOURCE, "\n")] = 0;
+
     char class[30];
     printf("Choose class: \n AC1/AC2/AC3/ Sleeper\n");
     scanf("%s", class); // asking the user for their desired choice of class
@@ -37,28 +49,30 @@ int main(){
     printf("Enter your personal details\n"); //asking the user the personal details as input
 
     printf("Passenger Name: \n");
-    scanf("%s", &passenger_name);
+    fgets(p.name, sizeof(p.name), stdin);
+    p.name[strcspn(p.name, "\n")] = 0;
+
     
     printf("Passenger Age: \n");
-    scanf("%d", &passenger_age); 
+    scanf("%d", &p.age); 
     
     printf("Gender (M/F/O): \n");
-    scanf("%s", &passenger_gender);
+    scanf("%s",p.gender);
 
     printf("Berth Preference (Lower/Upper/None): \n");
     char p_pref[10];
-    scanf("%s", p_pref);
+    scanf("%s", p.pref);
     
-    printf("Details recorded.\nAge: %d\nGender: %s\nPreference: %s\n", passenger_age, passenger_gender, p_pref);//Printing all the details taken as input from the user
+    printf("Details recorded.\nAge: %d\nGender: %s\nPreference: %s\n", p.age, p.gender, p.pref);//Printing all the details taken as input from the user
 
     printf("Payment\n");
-    printf("Ticket price: %2f\n", TICKET_PRICE); //proceeding towards the payment for the ticket
+    printf("Ticket price: %.2f\n", TICKET_PRICE); //proceeding towards the payment for the ticket
 
     char confirm[3]; //creating a character array to ask for confirmation for payment
     printf("DO you want to proceed with the payment?\n");
     scanf("%s", &confirm);
 
-    if (strcmp(confirm, "no") == 0) { // Check if 'confirm' is exactly "no"
+    if (strcasecmp(confirm, "no") == 0) { // Check if 'confirm' is exactly "no"
         printf("Payment cancelled.\n"); // Added for clarity
         return 0; // Exit the program
         }
@@ -72,7 +86,7 @@ int main(){
 
     printf("E- TICKET CONFIRMED!!\n");// Generate an e-ticket which will contain the neccessary information along with with PNR and confirmation
     printf("PNR: JRU40953486\n");
-    printf("Passanger: %s\n", passenger_name);
+    printf("Passanger: %s\n", p.name);
     printf("Date: %s\n", date_of_journey);
     printf("TO: %s\nFROM: %s\n", DESTINATION,SOURCE);
     printf("Class: %s\n", class);
